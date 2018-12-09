d43e8d60173b6e2b16bffef94e88283c87e552a4
Jean Monlong <jmonlong@ucsc.edu>
2018-12-09 09:29:58 -0800

    headers

--- python/reduceBib/reduceBib.py
+++ python/reduceBib/reduceBib.py
@@ -1,3 +1,19 @@
+# reduceBib.py reduces a .bib file to speed up its use by Pandoc
+# Copyright (C) 2018  Jean Monlong
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import re
 # import glob
