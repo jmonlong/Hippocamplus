077eca636108f9307ec0bd028903a0b2fb4f1f96
jmonlong <jmonlong@ucsc.edu>
2019-05-21 18:05:29 -0700

    cleanup and add missed pub

--- R/hgenPubs/get-pubmed-scopus.R
+++ R/hgenPubs/get-pubmed-scopus.R
@@ -2,7 +2,7 @@ library(rentrez)
 library(xml2)
 library(dplyr)
 
-## Functions to tranform "NULL" into "NA"
+## Tranforms "NULL" into "NA"
 nullToNA = function(x){
   ifelse(is.null(x), NA, x)
 }
@@ -47,29 +47,36 @@ fetchpmid = function(pmid){
   df
 }
 
-## Search potential publications and init objects
-pm.res = entrez_search(db='pubmed', term='(Human Genetics[Affiliation]) AND McGill[Affiliation]', retmax=2000)
+## List potential publications
+pm.res = entrez_search(db='pubmed', term='(Human Genetics[Affiliation]) AND McGill[Affiliation]')
+pm.res = entrez_search(db='pubmed', term='(Human Genetics[Affiliation]) AND McGill[Affiliation]', retmax=pm.res$count)
+
+## Publications with problems in PubMed but that are HGEN pubs
+force.pmids = '7647779'
+
+## Init objects
+pmids = c(pm.res$ids, force.pmids)
 res.l = list()
-save(pm.res, res.l, file="hgen-pubmed.RData")
+save(pmids, res.l, file="hgen-pubmed.RData")
 
 ## Fetch info for each candidate publication
-## Can be stopped and restarted without refetching everything
+## Ugly while loop but if there is an error (or if we want) it can be stopped
+## without losing anything. Just save what was done and continue when the
+## error is fixed (or later)
 load("hgen-pubmed.RData")
-
 pb = txtProgressBar(style=3)
-while(length(res.l) < length(pm.res$ids)){
-  if(length(res.l) %% 50 == 0) setTxtProgressBar(pb,length(res.l)/length(pm.res$ids))
-  res.l = c(res.l, list(fetchpmid(pm.res$ids[length(res.l)+1])))
+while(length(res.l) < length(pmids)){
+  if(length(res.l) %% 50 == 0) setTxtProgressBar(pb,length(res.l)/length(pmids))
+  res.l = c(res.l, list(fetchpmid(pmids[length(res.l)+1])))
 }
-
 close(pb)
-
 save(pm.res, res.l, file="hgen-pubmed.RData")
 
-
+## Data.frame with the info
 pm.df = do.call(rbind, res.l)
 
-## Check dates
+## Format the dates
+## Uniform month names, default day/month if missing
 monthConv = 1:12
 names(monthConv) = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
@@ -77,47 +84,55 @@ pm.df$month.num = as.numeric(pm.df$month)
 pm.df$month = ifelse(is.na(pm.df$month.num), monthConv[pm.df$month],
                      pm.df$month)
 pm.df$month = ifelse(is.na(pm.df$month), 1, pm.df$month)
+pm.df$month.num = NULL
 pm.df$day = ifelse(is.na(pm.df$day), 1, pm.df$day)
 pm.df$date = as.Date(paste(pm.df$year, pm.df$month, pm.df$day, sep='-'))
-pm.df$month.num = NULL
 
-## Catch when only the first author has affiliation
-## -> use this affiliations for all authors
+## When only the first author has affiliation, use this affiliation
+## for all authors
 fixSingleAff = function(df){
+  df$aff.single = TRUE
   if(nrow(df)>1 && !is.na(df$aff[1]) && all(is.na(df$aff[-1]))){
     df$aff = df$aff[1]
+    df$aff.single = FALSE
   }
   df
 }
 pm.df = pm.df %>% group_by(pmid) %>% do(fixSingleAff(.))
 
+## Annotate the length of the affiliation, if too long maybe not
+## specific to the author.
+pm.df$aff.nchar = nchar(pm.df$aff)
+
 ## Where are the NAs?
 ## apply(pm.df, 2, function(x) sum(is.na(x)))
 
-## Check Consortia like Care 4 Rare
+## Check that large consortia (e.g. Care 4 Rare) are not in author names.
 ## subset(pm.df, grepl('care', auth.lastname, ignore.case=TRUE))
 ## subset(pm.df, grepl('care', auth.firstname, ignore.case=TRUE))
 ## subset(pm.df, grepl('care', aff, ignore.case=TRUE) & is.na(auth.firstname))
 
-## Length of the affiliation, if too long maybe not specific to author
-pm.df$aff.nchar = nchar(pm.df$aff)
-
 ## HGEN author?
 pm.df$hgen = grepl("Human Genetics", pm.df$aff) & grepl("McGill", pm.df$aff)
+## Manual corrections
+pm.df$hgen[which(pm.df$pmid %in% force.pmids)] = TRUE
 
 ## Remove affiliation info and transform to one row per author/publication
 ## Also removes pubs with no HGEN authors
-pm.df = pm.df %>% group_by(pmid, auth.firstname, auth.lastname) %>%
+pm.df = pm.df %>%
+  group_by(pmid, auth.firstname, auth.lastname) %>%
   mutate(hgen=any(hgen), aff.nchar=max(aff.nchar)) %>%
   select(-aff) %>% unique %>%
   group_by(pmid) %>% filter(any(hgen, na.rm=TRUE))
 
-## Remove non journal articles
-pm.df$type %>% strsplit(split=';') %>% unlist %>% table %>% sort
-pm.df = pm.df %>% filter(grepl('Journal Article', type))
+## Search Scopus for the citation count for these PMIDs
+## https://www.scopus.com/search/form.uri?zone=TopNavBar&origin=searchadvanced&display=advanced
+pm.df %>% filter(hgen) %>% .$pmid %>% unique %>% paste0("PMID(",.,")") %>% paste(collapse=" OR ") %>% cat(file="pmids-hgen-forscopus.txt")
+## On scopus, export a csv with pmid and citation count
+scopus = read.csv('scopus-hgen.csv', as.is=TRUE, header=TRUE)
+colnames(scopus) = c('cited', 'link', 'pmid')
+scopus$link = NULL
+pm.df = merge(pm.df, scopus, all.x=TRUE)
 
 ## Write tsv file
 write.table(pm.df, file="hgen-pubmed.tsv", quote=FALSE, sep='\t', row.names=FALSE)
-
-## Search Scopus for the citation count (https://www.scopus.com/search/form.uri?zone=TopNavBar&origin=searchadvanced&display=advanced)
-pm.df %>% filter(hgen) %>% .$pmid %>% unique %>% paste0("PMID(",.,")") %>% paste(collapse=" OR ") %>% cat(file="pmids-hgen-forscopus.txt")
